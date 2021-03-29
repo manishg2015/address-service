668e2120e41c806b3fbc03046c8fa87ae0b2630a
Manish Garg <manishg2015@gmail.com>
2021-03-29 18:43:41 +0530

    Updated files
    Updated files

--- src/main/java/net/demo/myjava/microservices/addressservice/service/AddressService.java
+++ src/main/java/net/demo/myjava/microservices/addressservice/service/AddressService.java
@@ -1,17 +1,41 @@
 package net.demo.myjava.microservices.addressservice.service;
 
+import lombok.extern.slf4j.Slf4j;
 import net.demo.myjava.microservices.addressservice.model.Address;
 import net.demo.myjava.microservices.addressservice.model.AddressType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 @Service
+@Slf4j
+
 public class AddressService {
 
+    private final String key1;
+    private final String key2;
+
+    @Autowired
+    public AddressService(
+
+            @Value("${app.secret.key1}") String key1,
+            @Value("${app.secret.key2}") String key2
+    ) {
+        this.key1 = key1;
+        this.key2 = key2;
+        log.info("*****Secret key1 is :" + key1);
+        log.info("*****Secret key2 is :" + key2);
+    }
+
     public Flux<Address> getAddressByPersonId(String personId){
+
+        log.info("*****In Address service Secret key1 is :" + key1);
+        log.info("*****In Address service Secret key2 is :" + key2);
         ArrayList<Address> addressList = new ArrayList<>(Arrays.asList(new Address[]{
                 Address.builder().city("Bangalore").personId(personId).country("IND").state("KA").
                         type(AddressType.HOME).postalCode("560075").street("New Thiipasandara").id("12345").build(),
